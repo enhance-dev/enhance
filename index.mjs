6e5445b3eb19d76aec34016c8f6078955233589d
kj <kj@begin.com>
2023-08-29 14:50:18 -0700

    Tests passing

---
+++ index.mjs
@@ -0,0 +1,31 @@
+import morphdom from "morphdom"
+
+const MorphdomMixin = (superclass) => class extends superclass {
+  constructor() {
+    super()
+    this.process = this.process.bind(this)
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue !== newValue) {
+      this.process()
+    }
+  }
+
+  process() {
+    const tmp = this.render({
+      html: this.html,
+      state: this.state
+    })
+    const updated = document.createElement('div')
+    updated.innerHTML = tmp.trim()
+    morphdom(
+      this,
+      updated,
+      {
+        childrenOnly: true
+      }
+    )
+  }
+}
+export default MorphdomMixin
