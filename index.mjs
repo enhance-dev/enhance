a61324eb789fe1f673519248635474be8bfb3259
kj <kj@begin.com>
2023-10-31 16:02:55 -0700

    Update adds fallback order

--- index.mjs
+++ index.mjs
@@ -5,25 +5,41 @@ const EventHandlerMixin = (superclass) => class extends superclass {
 
   connectedCallback() {
     const htmlElementPrototype = Object.getOwnPropertyNames(HTMLElement.prototype)
-    const root = this.shadowRoot
-      ? this.shadowRoot
-      : this
     const attrs = this?.state?.attrs
+    const eventsToRemove = []
     Object.keys(attrs)
       .map(event => {
         if (htmlElementPrototype.indexOf(`on${event}`) !== -1) {
-          const target = attrs[event]
-            ? root.querySelector(attrs[event])
-            : root
-          target.addEventListener(event, this[event])
+          /*
+           * Elements we want to add event listeners to can either live in light DOM or shadow DOM.
+           * Slotted content and Custom Element content will live in light DOM so we use querySelector by default.
+           * If we don't find them there then we look in the shadowRoot using shadowRoot.querySelector
+           * otherwise we add the listener to the component instead of a nested element.
+           * Problem with adding a listener to the component is that there could be issues adding more than one event listener ( think blur, click and submit for instance )
+          */
+          const target = this.querySelector(attrs[event]) ||
+            this.shadowRoot.querySelector(attrs[event]) ||
+            this
+
+          if (target) {
+            target.addEventListener(event, this[event])
+            eventsToRemove.push({ target, event: this[event] })
+          }
         }
       })
 
     if(super.connectedCallback) {
       super.connectedCallback()
     }
+  }
 
+  disconnectedCallback() {
+    eventsToRemove.forEach(l => removeEventListener(l.event, l.target))
+    if (super.disconnectedCallback) {
+      super.disconnectedCallback()
+    }
   }
 }
 
 export default EventHandlerMixin
+
